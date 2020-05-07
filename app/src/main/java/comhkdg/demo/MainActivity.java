48e71c7b7b08cf02906327f958ad34959210e36c
Daem <945460266@qq.com>
2020-05-07 10:14:40 +0800

    添加demo

--- app/src/main/java/comhkdg/demo/MainActivity.java
+++ app/src/main/java/comhkdg/demo/MainActivity.java
@@ -17,8 +17,8 @@ public class MainActivity extends AppCompatActivity {
 
         TextView textView = findViewById(R.id.textView);
         LetterSideBar letterSideBar = findViewById(R.id.letterSideBar);
-//        String[] letter = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
-//        letterSideBar.setLetters(letter);
+        String[] letter = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        letterSideBar.setLetters(letter);
         letterSideBar.setOnTouchingTextListener((text) -> {
             if (text == null) {
                 textView.setVisibility(View.GONE);
